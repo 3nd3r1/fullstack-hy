669d980038fe164c996fd3d6924147d8125308d0
3nd3r1 <ender.spam.guru@gmail.com>
2023-02-02 15:24:30 +0200

    Tehtävät 3.1.-3.6.

---
+++ osa3/puhelinluettelo-server/index.js
@@ -0,0 +1,86 @@
+const express = require("express");
+const app = express();
+
+let persons = [
+	{
+		id: 1,
+		name: "Arto Hellas",
+		number: "040-123456",
+	},
+	{
+		id: 2,
+		name: "Ada Lovelace",
+		number: "39-44-5323523",
+	},
+	{
+		id: 3,
+		name: "Dan Abramov",
+		number: "12-43-234345",
+	},
+	{
+		id: 4,
+		name: "Mary Poppendick",
+		number: "39-23-6423122",
+	},
+];
+
+const generateId = () => {
+	let id = 1;
+	while (persons.find((person) => person.id === id)) {
+		id = Math.floor(Math.random() * 1e9);
+	}
+	return id;
+};
+
+app.use(express.json());
+
+app.get("/api/persons", (req, res) => {
+	res.json(persons);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+	const person = persons.find(
+		(person) => person.id === Number(req.params.id)
+	);
+	if (person) {
+		res.json(person);
+	} else {
+		res.status(404).end();
+	}
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+	persons = persons.filter((person) => person.id !== Number(req.params.id));
+
+	res.status(204).end();
+});
+
+app.post("/api/persons", (req, res) => {
+	const body = req.body;
+
+	if (!body.name || !body.number) {
+		return res
+			.status(400)
+			.json({ error: "name and number must be specified" });
+	}
+
+	if (persons.find((person) => person.name === body.name)) {
+		return res.status(400).json({ error: "name must be unique" });
+	}
+
+	newPerson = { id: generateId(), ...body };
+	persons = persons.concat(newPerson);
+
+	res.json(newPerson);
+});
+
+app.get("/info", (req, res) => {
+	res.send(
+		`Phonebook has info for ${persons.length} people <br></br> ${Date()}`
+	);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`);
+});
