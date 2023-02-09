3d843b1b18f81244e25f675cf758412799844935
3nd3r1 <ender.spam.guru@gmail.com>
2023-02-09 17:08:39 +0200

    Tehtävät 3.13.-3.14.

--- osa3/puhelinluettelo-server/index.js
+++ osa3/puhelinluettelo-server/index.js
@@ -1,39 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const Person = require("./models/person");
 
 const app = express();
-
-let persons = [
-	{
-		id: 1,
-		name: "Arto Hellas",
-		number: "040-123456",
-	},
-	{
-		id: 2,
-		name: "Ada Lovelace",
-		number: "39-44-5323523",
-	},
-	{
-		id: 3,
-		name: "Dan Abramov",
-		number: "12-43-234345",
-	},
-	{
-		id: 4,
-		name: "Mary Poppendick",
-		number: "39-23-6423122",
-	},
-];
-
-const generateId = () => {
-	let id = 1;
-	while (persons.find((person) => person.id === id)) {
-		id = Math.floor(Math.random() * 1e9);
-	}
-	return id;
-};
 
 morgan.token("body", (req, res) =>
 	JSON.stringify(req.body) !== "{}" ? JSON.stringify(req.body) : ""
@@ -49,24 +20,23 @@ app.use(
 app.use(express.json());
 
 app.get("/api/persons", (req, res) => {
-	res.json(persons);
+	Person.find({}).then((people) => res.json(people));
 });
 
 app.get("/api/persons/:id", (req, res) => {
-	const person = persons.find(
-		(person) => person.id === Number(req.params.id)
-	);
-	if (person) {
-		res.json(person);
-	} else {
-		res.status(404).end();
-	}
+	Person.findById(req.params.id).then((result) => {
+		if (result) {
+			res.json(result);
+		} else {
+			res.status(404).end();
+		}
+	});
 });
 
 app.delete("/api/persons/:id", (req, res) => {
-	persons = persons.filter((person) => person.id !== Number(req.params.id));
-
-	res.status(204).end();
+	Person.findByIdAndDelete(req.params.id).then((result) =>
+		res.status(204).end()
+	);
 });
 
 app.post("/api/persons", (req, res) => {
@@ -78,21 +48,21 @@ app.post("/api/persons", (req, res) => {
 			.json({ error: "name and number must be specified" });
 	}
 
-	if (persons.find((person) => person.name === body.name)) {
-		return res.status(400).json({ error: "name must be unique" });
-	}
+	const person = new Person({
+		name: body.name,
+		number: body.number,
+	});
 
-	newPerson = { id: generateId(), ...body };
-	persons = persons.concat(newPerson);
-
-	res.json(newPerson);
+	person.save().then((savedPerson) => res.json(savedPerson));
 });
 
+/*
 app.get("/info", (req, res) => {
 	res.send(
 		`Phonebook has info for ${persons.length} people <br></br> ${Date()}`
 	);
 });
+*/
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
