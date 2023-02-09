c6f1a1572e24a330b7604ba4a6d3a552329df88d
3nd3r1 <ender.spam.guru@gmail.com>
2023-02-09 17:46:27 +0200

    Tehtävät 3.15.-3.18.

--- osa3/puhelinluettelo-server/index.js
+++ osa3/puhelinluettelo-server/index.js
@@ -12,34 +12,57 @@ morgan.token("body", (req, res) =>
 
 app.use(cors());
 app.use(express.static("build"));
+app.use(express.json());
 app.use(
 	morgan(
 		":method :url :status :res[content-length] - :response-time ms :body"
 	)
 );
-app.use(express.json());
 
-app.get("/api/persons", (req, res) => {
-	Person.find({}).then((people) => res.json(people));
+app.get("/api/persons", (req, res, next) => {
+	Person.find({})
+		.then((people) => res.json(people))
+		.catch((error) => next(error));
 });
 
-app.get("/api/persons/:id", (req, res) => {
-	Person.findById(req.params.id).then((result) => {
-		if (result) {
-			res.json(result);
-		} else {
-			res.status(404).end();
-		}
-	});
+app.get("/api/persons/:id", (req, res, next) => {
+	Person.findById(req.params.id)
+		.then((person) => {
+			if (person) {
+				res.json(person);
+			} else {
+				res.status(404).end();
+			}
+		})
+		.catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (req, res) => {
-	Person.findByIdAndDelete(req.params.id).then((result) =>
-		res.status(204).end()
-	);
+app.delete("/api/persons/:id", (req, res, next) => {
+	Person.findByIdAndDelete(req.params.id)
+		.then((person) => res.status(204).end())
+		.catch((error) => next(error));
 });
 
-app.post("/api/persons", (req, res) => {
+app.put("/api/persons/:id", (req, res, next) => {
+	const body = req.body;
+
+	if (!body.name || !body.number) {
+		return res
+			.status(400)
+			.json({ error: "name and number must be specified" });
+	}
+
+	const person = {
+		name: body.name,
+		number: body.number,
+	};
+
+	Person.findByIdAndUpdate(req.params.id, person, { new: true })
+		.then((updatedPerson) => res.json(updatedPerson))
+		.catch((error) => next(error));
+});
+
+app.post("/api/persons", (req, res, next) => {
 	const body = req.body;
 
 	if (!body.name || !body.number) {
@@ -53,7 +76,10 @@ app.post("/api/persons", (req, res) => {
 		number: body.number,
 	});
 
-	person.save().then((savedPerson) => res.json(savedPerson));
+	person
+		.save()
+		.then((savedPerson) => res.json(savedPerson))
+		.catch((error) => next(error));
 });
 
 /*
@@ -63,6 +89,21 @@ app.get("/info", (req, res) => {
 	);
 });
 */
+
+const unknownEndpoint = (req, res) =>
+	res.status(404).send({ error: "unknown endpoint" });
+app.use(unknownEndpoint);
+
+const errorHandler = (error, req, res, next) => {
+	console.error(error.message);
+
+	if (error.name === "CastError") {
+		return res.status(400).send({ error: "malformatted id" });
+	}
+
+	next(error);
+};
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
