77d1d87538ee36686ab532b7aa02c1e339ae36a1
3nd3r1 <ender.spam.guru@gmail.com>
2023-02-09 13:37:57 +0200

    Tehtävät 3.9.-3.11.

--- osa3/puhelinluettelo-server/index.js
+++ osa3/puhelinluettelo-server/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
+
 const app = express();
 
 let persons = [
@@ -36,6 +38,8 @@ const generateId = () => {
 morgan.token("body", (req, res) =>
 	JSON.stringify(req.body) !== "{}" ? JSON.stringify(req.body) : ""
 );
+
+app.use(cors());
 app.use(
 	morgan(
 		":method :url :status :res[content-length] - :response-time ms :body"
@@ -89,7 +93,7 @@ app.get("/info", (req, res) => {
 	);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
 });
