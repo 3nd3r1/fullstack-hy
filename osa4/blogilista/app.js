feca81cff9a42978def075b56c9920fec46230e7
3nd3r1 <ender.spam.guru@gmail.com>
2023-02-15 14:04:31 +0200

    Tehtävät 4.1.-4.2.

---
+++ osa4/blogilista/app.js
@@ -0,0 +1,29 @@
+const config = require("./utils/config");
+const logger = require("./utils/logger");
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const blogsRouter = require("./controllers/blogs");
+
+const app = express();
+
+/* MongoDB database connection */
+mongoose.set("strictQuery", false);
+logger.info("connecting to", config.MONGODB_URI);
+mongoose
+	.connect(config.MONGODB_URI)
+	.then(() => {
+		logger.info("connected to MongoDB");
+	})
+	.catch((error) => {
+		logger.error("error connecting to MongoDB:", error.message);
+	});
+
+/* Express stuff */
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/blogs", blogsRouter);
+
+module.exports = app;
