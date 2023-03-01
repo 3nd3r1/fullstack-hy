8b0e2695caf92d72dacfc06798ce42714abdcf00
3nd3r1 <ender.spam.guru@gmail.com>
2023-03-01 17:10:10 +0200

    Tehtävä 6.9

--- osa6/redux-anecdotes/src/components/AnecdoteList.js
+++ osa6/redux-anecdotes/src/components/AnecdoteList.js
@@ -3,7 +3,11 @@ import { useDispatch, useSelector } from "react-redux";
 import { voteAnecdote } from "../reducers/anecdoteReducer";
 
 const AnecdoteList = () => {
-	const anecdotes = useSelector((state) => state);
+	const anecdotes = useSelector((state) =>
+		state.anecdotes.filter((anecdote) =>
+			anecdote.content.toLowerCase().includes(state.filter.toLowerCase())
+		)
+	);
 	const dispatch = useDispatch();
 
 	const vote = (id) => {
@@ -13,15 +17,26 @@ const AnecdoteList = () => {
 	return (
 		<div>
 			<h2>Anecdotes</h2>
-			{anecdotes.map((anecdote) => (
-				<div key={anecdote.id}>
-					<div>{anecdote.content}</div>
-					<div>
-						has {anecdote.votes}
-						<button onClick={() => vote(anecdote.id)}>vote</button>
-					</div>
-				</div>
-			))}
+			<ul>
+				{anecdotes.map((anecdote) => (
+					<li key={anecdote.id}>
+						<div>
+							<div>
+								<strong>{anecdote.content}</strong>
+							</div>
+							<div>
+								has {anecdote.votes}
+								<button
+									style={{ marginLeft: "5px" }}
+									onClick={() => vote(anecdote.id)}
+								>
+									vote
+								</button>
+							</div>
+						</div>
+					</li>
+				))}
+			</ul>
 		</div>
 	);
 };
