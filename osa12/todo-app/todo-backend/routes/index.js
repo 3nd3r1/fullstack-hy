6369b672a85ec99cb6cdd9fb9f63e14356b2949f
3nd3r1 <ender.spam.guru@gmail.com>
2024-06-14 13:51:20 +0300

    Exercise 12.10

--- osa12/todo-app/todo-backend/routes/index.js
+++ osa12/todo-app/todo-backend/routes/index.js
@@ -1,18 +1,25 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const redis = require("../redis");
 
-const configs = require('../util/config')
+const configs = require("../util/config");
 
-let visits = 0
+let visits = 0;
 
 /* GET index data. */
-router.get('/', async (req, res) => {
-  visits++
+router.get("/", async (req, res) => {
+    visits++;
 
-  res.send({
-    ...configs,
-    visits
-  });
+    res.send({
+        ...configs,
+        visits,
+    });
+});
+
+/* GET statistics */
+router.get("/statistics", async (_, res) => {
+    const added_todos = parseInt(await redis.getAsync("added_todos") ?? 0);
+    res.send({ added_todos });
 });
 
 module.exports = router;
